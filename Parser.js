08d6b6a23c379b3195d1bec33ef5cb1a552acae3
Kévin TIASSOU <64667821+ktiass@users.noreply.github.com>
2023-11-24 16:23:46 +0000

    Put data in one folder

--- Parser.js
+++ Parser.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 /**
  * Fonction pour analyser le contenu d'un fichier .cru
@@ -7,26 +7,26 @@ const path = require('path');
  * @returns {Object[]} - Un tableau d'objets représentant les données analysées
  */
 function parseCRUFile(filePath) {
-    const content = fs.readFileSync(filePath, 'utf8');
-    const lines = content.split('\n');
+  const content = fs.readFileSync(filePath, "utf8");
+  const lines = content.split("\n");
 
-    const parsedData = [];
-    let currentSection = null;
+  const parsedData = [];
+  let currentSection = null;
 
-    lines.forEach(line => {
-        if (line.startsWith('+')) {
-            // Début d'une nouvelle section
-            currentSection = line.substring(1).trim();
-        } else if (currentSection && line.trim()) {
-            // Traitement des données de la section
-            const data = parseDataLine(line);
-            if (data) {
-                parsedData.push({ section: currentSection, data });
-            }
-        }
-    });
+  lines.forEach((line) => {
+    if (line.startsWith("+")) {
+      // Début d'une nouvelle section
+      currentSection = line.substring(1).trim();
+    } else if (currentSection && line.trim()) {
+      // Traitement des données de la section
+      const data = parseDataLine(line);
+      if (data) {
+        parsedData.push({ section: currentSection, data });
+      }
+    }
+  });
 
-    return parsedData;
+  return parsedData;
 }
 
 /**
@@ -35,36 +35,44 @@ function parseCRUFile(filePath) {
  * @returns {Object|null} - Un objet représentant les données ou null si non analysable
  */
 function parseDataLine(line) {
-    // Séparation des composants de la ligne
-    const components = line.split(',');
+  // Séparation des composants de la ligne
+  const components = line.split(",");
 
-    // Assurer que la ligne contient suffisamment de composants
-    if (components.length < 6) {
-        return null;
-    }
+  // Assurer que la ligne contient suffisamment de composants
+  if (components.length < 6) {
+    return null;
+  }
 
-    // Extraction des données
-    const courseId = components[0].trim();
-    const courseType = components[1].trim();
-    const participants = components[2].includes('=') ? components[2].split('=')[1].trim() : null;
-    const timeInfo = components[3].split(' ');
-    const day = timeInfo[0].includes('=') ? timeInfo[0].split('=')[1].trim() : null;
-    const hours = timeInfo[1].trim();
-    const frequency = components[4].includes('=') ? components[4].split('=')[1].trim() : null;
-    const room = components[5].includes('=') ? components[5].split('=')[1].split('//')[0].trim() : null;
+  // Extraction des données
+  const courseId = components[0].trim();
+  const courseType = components[1].trim();
+  const participants = components[2].includes("=")
+    ? components[2].split("=")[1].trim()
+    : null;
+  const timeInfo = components[3].split(" ");
+  const day = timeInfo[0].includes("=")
+    ? timeInfo[0].split("=")[1].trim()
+    : null;
+  const hours = timeInfo[1].trim();
+  const frequency = components[4].includes("=")
+    ? components[4].split("=")[1].trim()
+    : null;
+  const room = components[5].includes("=")
+    ? components[5].split("=")[1].split("//")[0].trim()
+    : null;
 
-    return {
-        courseId,
-        courseType,
-        participants,
-        day,
-        hours,
-        frequency,
-        room
-    };
+  return {
+    courseId,
+    courseType,
+    participants,
+    day,
+    hours,
+    frequency,
+    room,
+  };
 }
 
 // Exemple d'utilisation
-const filePath = './AB/edt.cru';
+const filePath = ".data/AB/edt.cru";
 const parsedData = parseCRUFile(filePath);
 console.log(parsedData);
